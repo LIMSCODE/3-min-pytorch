3819680f73f09d6262f3f78bb25224beeb43025c
LIMSCODE <101355854+LIMSCODE@users.noreply.github.com>
2023-10-25 21:47:06 +0900

    Update resnet.py

--- 05-이미지_처리능력이_탁월한_CNN/resnet.py
+++ 05-이미지_처리능력이_탁월한_CNN/resnet.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # 신경망 깊게 쌓아 컬러 데이터셋에 적용하기
-# Convolutional Neural Network (CNN) 을 쌓아올려 딥한 러닝을 해봅시다.
+# # 신경망 깊게 쌓아 컬러 데이터셋에 적용하기 
+# Convolutional Neural Network (CNN) 을 쌓아올려 딥한 러닝을 해봅시다. 
+- 여러단계의 신경망을 거치며 최초입력 이미지에 대한 정보 소실 -> 작은블록인 Residual블록으로 나누어, 출력에 전전층의 입력을 더하는 ResNet으로 해결 
+- Residual 블록 = BasicBlock 파이토치 모듈로 정의하여 사용 (nn.Module이용하여 모듈 위에 모듈 쌓음)
 
 import torch
 import torch.nn as nn
@@ -10,21 +12,16 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torchvision import transforms, datasets, models
 
-
 USE_CUDA = torch.cuda.is_available()
 DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
 
-
 # ## 하이퍼파라미터 
-
 EPOCHS     = 300
 BATCH_SIZE = 128
 
-
 # ## 데이터셋 불러오기
-
 train_loader = torch.utils.data.DataLoader(
-    datasets.CIFAR10('./.data',
+    datasets.CIFAR10('./.data',            // CIFAR-10데이터셋 사용
                    train=True,
                    download=True,
                    transform=transforms.Compose([
@@ -45,26 +42,25 @@ test_loader = torch.utils.data.DataLoader(
 
 
 # ## ResNet 모델 만들기
-
 class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
+        super(BasicBlock, self).__init__()        //BasicBlock = Residual블록 의미
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3,
                                stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = nn.BatchNorm2d(planes)        //배치정규화 수행 : 학습률이 너무높으면 기울기 소실되는것 예방하여 안정화
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
-            self.shortcut = nn.Sequential(
+            self.shortcut = nn.Sequential(    //nn.Sequential : nn.Module을 하나의 모듈로 묶는역할
                 nn.Conv2d(in_planes, planes,
                           kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(planes)
             )
 
-    def forward(self, x):
+    def forward(self, x):                    //x를 컨볼루션, 배치정규화, 활성화함수 거치고 입력x를 self.shortcut을 거쳐 크기를 같게하고 활성화함수를 더함
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
@@ -72,7 +68,7 @@ class BasicBlock(nn.Module):
         return out
 
 
-class ResNet(nn.Module):
+class ResNet(nn.Module):                    //모델 정의 : 이미지를받아 컨볼루션, 배치정규화를 거친후 / 여러 BasicBlock층을 통과 / 풀링, 신경망 거쳐 예측 출력 
     def __init__(self, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 16
@@ -89,8 +85,8 @@ class ResNet(nn.Module):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(BasicBlock(self.in_planes, planes, stride))
-            self.in_planes = planes
+            layers.append(BasicBlock(self.in_planes, planes, stride))    //##BasicBlock함수 : self.make_layer()함수를 통해 ResNet모델의 주요층을 이룸.
+            self.in_planes = planes                                      //self.make_layer()는 nn.Sequential도구로, 여러 BasicBlock을 모듈하나로 묶어줌.
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -105,7 +101,6 @@ class ResNet(nn.Module):
 
 
 # ## 준비
-
 model = ResNet().to(DEVICE)
 optimizer = optim.SGD(model.parameters(), lr=0.1,
                       momentum=0.9, weight_decay=0.0005)
@@ -116,7 +111,6 @@ print(model)
 
 
 # ## 학습하기
-
 def train(model, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -129,7 +123,6 @@ def train(model, train_loader, optimizer, epoch):
 
 
 # ## 테스트하기
-
 def evaluate(model, test_loader):
     model.eval()
     test_loss = 0
@@ -154,7 +147,6 @@ def evaluate(model, test_loader):
 
 # ## 코드 돌려보기
 # 자, 이제 모든 준비가 끝났습니다. 코드를 돌려서 실제로 훈련이 되는지 확인해봅시다!
-
 for epoch in range(1, EPOCHS + 1):
     scheduler.step()
     train(model, train_loader, optimizer, epoch)
